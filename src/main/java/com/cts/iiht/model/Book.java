cccb0d1b9c8b3d66fc3a5c4d02d1993faa7a1c7a
mani-nandipaati <nandipaati.mani@gmail.com>
2019-01-05 10:24:53 +0530

    First Commit

---
+++ src/main/java/com/cts/iiht/model/Book.java
@@ -0,0 +1,63 @@
+package com.cts.iiht.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+
+public class Book implements Serializable{
+
+	private static final long serialVersionUID = -5629942468340856617L;
+	
+	
+	long bookId;
+	
+	
+	String title;
+	
+	
+	double price;
+	
+	
+	int volume;
+	
+	
+	LocalDate publishDate;
+	
+	public long getBookId() {
+		return bookId;
+	}
+	public void setBookId(long bookId) {
+		this.bookId = bookId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getVolume() {
+		return volume;
+	}
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	public LocalDate getPublishDate() {
+		return publishDate;
+	}
+	public void setPublishDate(LocalDate publishDate) {
+		this.publishDate = publishDate;
+	}
+	@Override
+	public String toString() {
+		return "Book [bookId=" + bookId + ", title=" + title + ", price=" + price + ", volume=" + volume
+				+ ", publishDate=" + publishDate + "]";
+	}
+	
+	
+}
